f30ef488efc78ecd83ede502a35a32a7f98fe976
azashour <Abdulaziz@ashour.com>
2022-10-22 19:14:21 -0700

    the geo location API that gives JSON struct of longitud and latitude

--- App.js
+++ App.js
@@ -1,20 +1,45 @@
 import { StatusBar } from 'expo-status-bar';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { db } from './firebaseConfig';
-import Home from './screens/Home';
+import Home from './Screens/Home';
+import * as Location from 'expo-location';
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
 
-  useEffect(() => {
+  const [location, setLocation] = useState(null);
+  const [errorMsg, setErrorMsg] = useState(null);
 
-  }, [])
+  useEffect(() => {
+    (async () => {
+
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== 'granted') {
+        setErrorMsg('Permission to access location was denied');
+        return;
+      }
+
+      let location = await Location.getLastKnownPositionAsync({});
+      setLocation(location);
+    })();
+  }, []);
+
+  let text = 'Waiting..';
+  if (errorMsg) {
+    text = errorMsg;
+  } else if (location) {
+    text = JSON.stringify(location);
+  }
 
   return (
+
+    // <View style = {styles.container}>
+    //   <Text style = {styles.baseText}>{text}</Text>
+    // </View>
 
     <NavigationContainer>
       <Stack.Navigator>
@@ -32,4 +57,8 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  baseText: {
+    fontSize: 10
+
+  }
 });
